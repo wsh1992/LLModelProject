e8972dcc45a5915f04f32ce966479ea4ee0b5f28
wsh1992 <1576326811@qq.com>
2026-03-24 20:48:57 +0800

    first commit

---
+++ src/main/java/com/example/LLModel/LlModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.LLModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LLModel/LlModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LLModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
